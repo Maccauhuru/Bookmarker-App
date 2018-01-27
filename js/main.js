bf5967a577e8f2cc0ca205f6c0ae9ac382f54cdc
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-01-26 20:10:25 -0600

    add and delete bmarks f(x)'s

--- js/main.js
+++ js/main.js
@@ -6,12 +6,25 @@ const savebookmark = (e) => {
     //save the bookmark
     let siteName = document.getElementById('siteName').value;
     let siteAddress = document.getElementById("siteAddress").value;
-   
+
+    if(!siteName || !siteAddress){
+        alert("Fields cannot be blank");
+        return false;
+    }
+   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+   var regex = new RegExp(expression);
+
+   if(!siteAddress.match(regex)){
+alert("Error , enter a valid URL");
+return false;
+   }
+
+
     //create object array to store on local storage
     let bookmark = {
         name: siteName,
         url: siteAddress
-    }
+    };
 
     //test if bookmark object is null in localStorage
     if(localStorage.getItem('bookmarks') == null){
@@ -28,10 +41,47 @@ const savebookmark = (e) => {
 
     //prevent form from submitting
     e.preventDefault();
+
+    //re-fetch bookmarks
+    fetchBookmarks();
 };
 
 
 myForm.addEventListener('submit',savebookmark);
+
+//Delete bookmarks
+const deleteBookmark = (url)=>{
+//get bmarks from local storage
+let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+for (let i = 0; i < bookmarks.length; i++) {
+if(bookmarks[i].url ==url){
+   bookmarks.splice(i,1);
+}
+}
+localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+
+//re-fetch bookmarks
+fetchBookmarks();
+};
+
+
+//function to fetch bookmarks
+const fetchBookmarks = () =>{
+let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+let bookmarksResults = document.getElementById("bookmarksResults");
+bookmarksResults.innerHTML ="";
+for(let i=0; i<bookmarks.length;i++){
+let name = bookmarks[i].name;
+let url = bookmarks[i].url;
+bookmarksResults.innerHTML+='<div class="well">'+
+                            '<h3>'+name+
+                            '<a class="btn btn-default" target="_blank" href="'+url+'">visit</a> '+
+                            '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">delete</a> ' +
+                            '</h3>'+
+                            '</div>';
+}
+}
+
 
 
 
