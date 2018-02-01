dd2f2c75c1e92ca823e20b0641f2930807672260
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-02-01 16:56:09 -0600

    add validation and clear f(x)s

--- js/main.js
+++ js/main.js
@@ -7,18 +7,9 @@ const savebookmark = (e) => {
     let siteName = document.getElementById('siteName').value;
     let siteAddress = document.getElementById("siteAddress").value;
 
-    if(!siteName || !siteAddress){
-        alert("Fields cannot be blank");
+    if (!validateForm(siteName, siteAddress)) {
         return false;
     }
-   var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
-   var regex = new RegExp(expression);
-
-   if(!siteAddress.match(regex)){
-alert("Error , enter a valid URL");
-return false;
-   }
-
 
     //create object array to store on local storage
     let bookmark = {
@@ -27,17 +18,19 @@ return false;
     };
 
     //test if bookmark object is null in localStorage
-    if(localStorage.getItem('bookmarks') == null){
+    if (localStorage.getItem('bookmarks') == null) {
         let bookmarks = [];
         bookmarks.push(bookmark)
-        localStorage.setItem('bookmarks',JSON.stringify(bookmarks));
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
     } else {
         //get items from localStorage
-       let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-       bookmarks.push(bookmark);
+        let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+        bookmarks.push(bookmark);
         //reset to localStorage as string
-       localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+        localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     }
+    //clear the form
+    myForm.reset();
 
     //prevent form from submitting
     e.preventDefault();
@@ -47,42 +40,56 @@ return false;
 };
 
 
-myForm.addEventListener('submit',savebookmark);
+myForm.addEventListener('submit', savebookmark);
 
 //Delete bookmarks
-const deleteBookmark = (url)=>{
-//get bmarks from local storage
-let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-for (let i = 0; i < bookmarks.length; i++) {
-if(bookmarks[i].url ==url){
-   bookmarks.splice(i,1);
-}
-}
-localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+const deleteBookmark = (url) => {
+    //get bmarks from local storage
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    for (let i = 0; i < bookmarks.length; i++) {
+        if (bookmarks[i].url == url) {
+            bookmarks.splice(i, 1);
+        }
+    }
+    localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
 
-//re-fetch bookmarks
-fetchBookmarks();
+    //re-fetch bookmarks
+    fetchBookmarks();
 };
 
 
 //function to fetch bookmarks
-const fetchBookmarks = () =>{
-let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
-let bookmarksResults = document.getElementById("bookmarksResults");
-bookmarksResults.innerHTML ="";
-for(let i=0; i<bookmarks.length;i++){
-let name = bookmarks[i].name;
-let url = bookmarks[i].url;
-bookmarksResults.innerHTML+='<div class="well">'+
-                            '<h3>'+name+
-                            '<a class="btn btn-default" target="_blank" href="'+url+'">visit</a> '+
-                            '<a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">delete</a> ' +
-                            '</h3>'+
-                            '</div>';
-}
+const fetchBookmarks = () => {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    let bookmarksResults = document.getElementById("bookmarksResults");
+    bookmarksResults.innerHTML = "";
+    for (let i = 0; i < bookmarks.length; i++) {
+        let name = bookmarks[i].name;
+        let url = bookmarks[i].url;
+        bookmarksResults.innerHTML += '<div class="well">' +
+            '<h3>' + name +
+            '<a class="btn btn-default" target="_blank" href="' + url + '">visit</a> ' +
+            '<a onclick="deleteBookmark(\'' + url + '\')" class="btn btn-danger" href="#">delete</a> ' +
+            '</h3>' +
+            '</div>';
+    }
 }
 
+//validate the form input submissions before saving to localStorage
+function validateForm(siteName,siteAddress) {
+    if (!siteName || !siteAddress) {
+        alert("Fields cannot be blank");
+        return false;
+    }
+    var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+    var regex = new RegExp(expression);
 
+    if (!siteAddress.match(regex)) {
+        alert("Error , please enter a valid URL");
+        return false;
+    }
+    return true;
+}
 
 
 
